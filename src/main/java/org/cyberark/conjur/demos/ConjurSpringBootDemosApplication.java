012d6266950ef6c939a73372fe6e52f154edbf43
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-03 22:09:52 +0200

    project init

---
+++ src/main/java/org/cyberark/conjur/demos/ConjurSpringBootDemosApplication.java
@@ -0,0 +1,13 @@
+package org.cyberark.conjur.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjurSpringBootDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjurSpringBootDemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cyberark/conjur/demos/ConjurSpringBootDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyberark.conjur.demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjurSpringBootDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
