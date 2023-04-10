a7a4e5ae4490b1ec173e155231675ea0f6855882
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-10 14:06:00 +0200

    native-image updates

---
+++ src/test/java/org/cyberark/conjur/demos/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package org.cyberark.conjur.demos;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
