1e486ca05feb868ae45f53769115f13720edf462
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-10 22:35:45 +0200

    samples update

--- src/main/java/org/cyberark/conjur/demos/common/RefreshableDatasource.java
+++ src/main/java/org/cyberark/conjur/demos/common/RefreshableDatasource.java
@@ -2,13 +2,12 @@ package org.cyberark.conjur.demos.common;
 
 import javax.sql.DataSource;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,11 +35,9 @@ public class RefreshableDatasource {
 	@Bean
 	@RefreshScope
 	public DataSource getDatasource() {
-		LOGGER.debug("Conjur Refresh Enabled");
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl(dataSourceProperties.getUrl());
-		config.setUsername(dataSourceProperties.getUsername());
-		config.setPassword(dataSourceProperties.getPassword());
-		return new HikariDataSource(config);
+		LOGGER.debug("Database Refresh Triggered");
+		return DataSourceBuilder.create().url(dataSourceProperties.getUrl())
+				.username(dataSourceProperties.getUsername())
+				.password(dataSourceProperties.getPassword()).build();
 	}
 }
