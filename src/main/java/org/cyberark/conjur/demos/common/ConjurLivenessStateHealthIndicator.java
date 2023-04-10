50e8ca40532ac265e209f0135163b8650d89b8d1
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-11 00:19:37 +0200

    samples update

--- src/main/java/org/cyberark/conjur/demos/common/ConjurLivenessStateHealthIndicator.java
+++ src/main/java/org/cyberark/conjur/demos/common/ConjurLivenessStateHealthIndicator.java
@@ -5,49 +5,36 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.actuate.availability.LivenessStateHealthIndicator;
-import org.springframework.boot.availability.ApplicationAvailability;
-import org.springframework.boot.availability.AvailabilityState;
-import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 /**
  * @author bnasslahsen
  */
 @Component
-public class ConjurLivenessStateHealthIndicator extends LivenessStateHealthIndicator {
+public class ConjurLivenessStateHealthIndicator implements HealthIndicator {
 
 	private final String scriptPath = "/conjur/status/conjur-secrets-unchanged.sh";
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConjurLivenessStateHealthIndicator.class);
 
-	public ConjurLivenessStateHealthIndicator(ApplicationAvailability availability) {
-		super(availability);
-	}
-
-	protected AvailabilityState getState(ApplicationAvailability applicationAvailability) {
-		if (secretsChanged())
-			return LivenessState.BROKEN;
-		return applicationAvailability.getLivenessState();
-	}
-
-	private boolean secretsChanged() {
+	@Override
+	public Health health() {
 		ProcessBuilder pb = new ProcessBuilder(scriptPath);
 		try {
 			Process p = pb.start();
 			int exitCode = p.waitFor();
 			if (exitCode == 0) {
 				LOGGER.debug("No password change");
-				return false;
-			}
-			else {
+				return Health.up().build();
+			} else {
 				LOGGER.debug("Password Change detected");
-				return true;
+				return Health.down().withDetail("exitCode", exitCode).build();
 			}
-		}
-		catch (IOException | InterruptedException e) {
+		} catch (IOException | InterruptedException e) {
 			LOGGER.error(e.getMessage(), e);
-			return false;
+			return Health.down(e).build();
 		}
 	}
 }
