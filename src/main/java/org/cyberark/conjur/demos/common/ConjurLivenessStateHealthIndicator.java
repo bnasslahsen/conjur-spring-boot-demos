c4fd24679578816570744f2a4b8bd74f0ef25008
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-10 23:24:04 +0200

    samples update

--- src/main/java/org/cyberark/conjur/demos/common/ConjurLivenessStateHealthIndicator.java
+++ src/main/java/org/cyberark/conjur/demos/common/ConjurLivenessStateHealthIndicator.java
@@ -2,6 +2,9 @@ package org.cyberark.conjur.demos.common;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -14,18 +17,22 @@ public class ConjurLivenessStateHealthIndicator implements HealthIndicator {
 
 	private final String scriptPath = "/conjur/status/conjur-secrets-unchanged.sh";
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConjurLivenessStateHealthIndicator.class);
+
 	@Override
 	public Health health() {
 		ProcessBuilder pb = new ProcessBuilder(scriptPath);
 		try {
 			Process p = pb.start();
 			int exitCode = p.waitFor();
+			LOGGER.debug("exitCode: " + exitCode);
 			if (exitCode == 0) {
 				return Health.up().build();
 			} else {
 				return Health.down().withDetail("exitCode", exitCode).build();
 			}
 		} catch (IOException | InterruptedException e) {
+			LOGGER.error( e.getMessage() ,  e);
 			return Health.down(e).build();
 		}
 	}
