c6f50983d857a8edf8e280c816bdfbe92747f7e9
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-11 14:39:28 +0200

    samples update

--- src/main/conjur/load-app-policies.sh
+++ src/main/conjur/load-app-policies.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -a
+source "./../../../.env"
+set +a
+
 conjur policy update -f data-branch.yml -b root >> data-branch.log
-conjur policy update -f app-hosts.yml -b data/bnl >> app-hosts.log
-conjur policy update -f host-grants.yml -b root >> host-grants.log
+
+envsubst < app-hosts.yml > app-hosts.yml.tmp
+conjur policy update -b data/bnl -f app-hosts.yml.tmp >> app-hosts.log
+rm app-hosts.yml.tmp
+
+envsubst < host-grants.yml > host-grants.yml.tmp
+conjur policy update -b root -f host-grants.yml.tmp
+rm host-grants.yml.tmp
 
 conjur variable set -i data/bnl/ocp-apps/url -v "jdbc:h2:mem:testdb"
 conjur variable set -i data/bnl/ocp-apps/password -v "vault-password"
