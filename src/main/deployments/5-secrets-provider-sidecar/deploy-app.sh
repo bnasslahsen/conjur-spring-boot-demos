75415ad60c5c58323033a5aa642fd3ca17c478ec
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-30 13:16:06 +0200

    samples update

--- src/main/deployments/5-secrets-provider-sidecar/deploy-app.sh
+++ src/main/deployments/5-secrets-provider-sidecar/deploy-app.sh
@@ -9,17 +9,11 @@ kubectl config set-context --current --namespace="$APP_NAMESPACE"
 kubectl delete serviceaccount demo-app-secrets-provider-sidecar-sa --ignore-not-found=true
 kubectl create serviceaccount demo-app-secrets-provider-sidecar-sa
 
-envsubst < roles.yaml | kubectl replace --force -f -
-
 # DB SECRETS
 envsubst < k8s-secrets.yml | kubectl replace --force -f -
 
 # Service account role binding
 envsubst < service-account-role.yml | kubectl replace --force -f -
-
-# DEPLOYMENT
-
-envsubst < watcher.yml | kubectl replace --force -f -
 
 envsubst < deployment.yml | kubectl replace --force -f -
 if ! kubectl wait deployment demo-app-secrets-provider-sidecar --for condition=Available=True --timeout=90s
