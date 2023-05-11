30ebc52ad1da8a61ee9a52bec42a7e9fbe6c876c
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-05-11 18:50:18 +0200

    changing log level

--- src/main/java/org/cyberark/conjur/demos/common/RefreshableDatasource.java
+++ src/main/java/org/cyberark/conjur/demos/common/RefreshableDatasource.java
@@ -7,19 +7,17 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author bnasslahsen
  */
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class RefreshableDatasource {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RefreshableDatasource.class);
 
 	//@RefreshScope
-	@Bean
+	//@Bean
 	public DataSource getDatasource(DataSourceProperties dataSourceProperties) {
 		LOGGER.debug("Database Refresh Triggered");
 		return DataSourceBuilder.create().url(dataSourceProperties.getUrl())
