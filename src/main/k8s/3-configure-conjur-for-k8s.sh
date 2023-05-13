d07c946ad8f98ab30ee99168e3a0a5f3ef0f6ce2
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-05-13 19:57:42 +0200

    samples update

--- src/main/k8s/3-configure-conjur-for-k8s.sh
+++ src/main/k8s/3-configure-conjur-for-k8s.sh
@@ -5,7 +5,7 @@ source "./../../../.env"
 source kube-info.txt
 set +a
 
-conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/public-keys -v "{\"type\":\"jwks\", \"value\":$(cat jwks.json)}"
+conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/jwks-uri -v $JWKS_URI
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/issuer -v $SA_ISSUER
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/token-app-property -v "sub"
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/identity-path -v "data/bnl/ocp-apps"
